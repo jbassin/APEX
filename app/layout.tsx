5f341e7d9d3888de84eb49e7924aaef89769aa69
Joshua Bassin <josh.r.bassin@gmail.com>
2023-08-02 15:03:56 -0400

    neater

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,22 @@
-import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "APEX",
-  description: "a new ttrpg",
+    title: "APEX",
+    description: "a new ttrpg",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en" data-theme="night" className="no-scrollbar">
+            <body className={inter.className}>{children}</body>
+        </html>
+    );
 }
